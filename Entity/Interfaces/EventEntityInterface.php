a54b2d2e0158e8fa8d985073c1349ca4f466e7e3
Farshad Asgharzade <Farshad_asgharzade@hotmail.com>
2018-07-17 16:02:36 +0430

    add first file like composer and ...

---
+++ Entity/Interfaces/EventEntityInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * This file is part of fullcalendarbundle
+ * Copyrighted by Narmafzam (Farzam Webnegar Sivan Co.), info@narmafzam.com
+ * Created by farshad
+ * Date: 7/17/18
+ */
+
+namespace Narmafzam\ArchiveBundle\Entity\Interfaces;
+
+interface EventEntityInterface
+{
+
+}
---
+++ FullCalendarBundle.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * This file is part of fullcalendarbundle
+ * Copyrighted by Narmafzam (Farzam Webnegar Sivan Co.), info@narmafzam.com
+ * Created by farshad
+ * Date: 7/17/18
+ */
+
+namespace Narmafzam\ArchiveBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Class FullCalendarBundle
+ * @package Narmafzam\ArchiveBundle
+ */
+class FullCalendarBundle extends Bundle
+{
+
+}
